af92159d600afff27e4a96fd7ecddf952859ed1c
ZauberNerd <zaubernerd@zaubernerd.de>
2018-05-16 12:38:35 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const { resolve, join, dirname } = require('path');
+const { readFileSync: readFile } = require('fs');
+
+const { sync: findUp } = require('find-up');
+const { create: { sync: createResolver } } = require('enhanced-resolve');
+const isBuiltin = require('is-builtin-module');
+
+const isESNext = path => {
+    return !path.includes('node_modules') ||
+        /"((e|j)snext(:[a-z]+)?|module)": ?"/m.test(
+            readFile(findUp('package.json', { cwd: dirname(path) }), 'utf8')
+        );
+};
+
+const isExternal = () => {
+    const resolve = createResolver();
+    const shouldBeBundled = (context, request) => {
+        if (isBuiltin(request)) return false;
+        try {
+            const resolved = resolve(context, request);
+            return isESNext(resolved);
+        } catch (_) {
+            return true;
+        }
+    };
+    return (context, request, callback) => {
+        if (shouldBeBundled(context, request)) {
+            callback();
+        } else {
+            callback(null, 'commonjs ' + request);
+        }
+    };
+};
+
+module.exports = {
+    name: 'node',
+    target: 'node',
+    mode: 'development',
+    entry: require.resolve('./index.js'),
+    output: {
+        path: join(__dirname, 'dist'),
+        filename: 'server.js',
+        libraryTarget: 'commonjs2',
+    },
+    resolve: {
+        extensions: ['.js'],
+        mainFields: [
+            'esnext',
+            'jsnext',
+            'esnext:main',
+            'jsnext:main',
+            'module',
+            'main',
+        ],
+    },
+    externals: isExternal(),
+};
